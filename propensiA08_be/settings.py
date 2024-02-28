7dd8def61fdf96dc5a8aa2bbf98006ffbe7730f7
Rafito Humam Abrar <rafito.humam@ui.ac.id>
2024-02-28 15:46:23 +0700

    App Initialization

--- propensiA08_be/settings.py
+++ propensiA08_be/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pabrik',
+    'gudang',
+    'barang',
+    'user',
 ]
 
 MIDDLEWARE = [
