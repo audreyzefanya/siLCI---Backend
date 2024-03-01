ef02603a8f6fafe5bcfee49980e6c4730123ee3b
rafitohumam <rafito.humam@ui.ac.id>
2024-03-01 14:54:14 +0700

    Unifying Pabrik database table field

--- pabrik/models.py
+++ pabrik/models.py
@@ -8,13 +8,13 @@ class Pabrik(models.Model):
     alamat = models.CharField(max_length=200)
 
 class BarangPabrik(models.Model):
-    id_barang = models.ForeignKey('barang.Barang', on_delete=models.CASCADE, primary_key=True, default=uuid.uuid4, editable=False, related_name='barang')
-    id_pabrik = models.ForeignKey('Pabrik', on_delete=models.CASCADE, default=uuid.uuid4, related_name='pabrik')
-    stok = models.IntegerField()
+    barang = models.ForeignKey('barang.Barang', on_delete=models.CASCADE, default=uuid.uuid4, primary_key=True, related_name='barang_pabrik')
+    pabrik = models.ForeignKey('Pabrik', on_delete=models.CASCADE, default=uuid.uuid4, related_name='pabrik')
+    stok = models.IntegerField(default = 0)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['id_barang', 'id_pabrik'], name='unique_barang_pabrik'
+                fields=['barang', 'pabrik'], name='unique_barang_pabrik'
             )
         ]
