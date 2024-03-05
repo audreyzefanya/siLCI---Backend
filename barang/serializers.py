f9f327fca1b80ef4fbbee3fbdf14070d0e2bf76e
rama.tridigdaya <rama.tridigdaya@ui.ac.id>
2024-03-05 20:39:47 +0700

    Adding Merk Serializer

--- barang/serializers.py
+++ barang/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
+class MerkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Merk
+        fields = ('id', 'nama')
+
 class BarangSerializer(serializers.ModelSerializer):
+    merk = MerkSerializer()
+    
     class Meta:
         model = Barang
         fields = '__all__'
