c48c87719030ee371e2d84126693901a77c9af7e
natasyaashil <natasya.ashil@ui.ac.id>
2024-03-07 15:57:39 +0700

    benerin merk 2

--- barang/serializers.py
+++ barang/serializers.py
@@ -9,11 +9,21 @@ class MerkSerializer(serializers.ModelSerializer):
         fields = ('id', 'nama')
 
 class BarangSerializer(serializers.ModelSerializer):
-    merk = serializers.PrimaryKeyRelatedField(queryset=Merk.objects.all())
-    
+    merk = MerkSerializer(read_only=True)
+    merk_id = serializers.PrimaryKeyRelatedField(
+        queryset=Merk.objects.all(), write_only=True, source='merk'
+    )
+
     class Meta:
         model = Barang
         fields = '__all__'
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
+
 
 class PerusahaanSerializer(serializers.ModelSerializer):
     logo_url = serializers.SerializerMethodField()
