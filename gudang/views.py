0d777e8b63af23e8c62090da3141c9fffedbf47e
audreyzefanya <audrey.zefanya@ui.ac.id>
2024-05-02 16:23:21 +0700

    Jenid Gudang

--- gudang/views.py
+++ gudang/views.py
@@ -82,6 +82,7 @@ class BarangGudangViewSet(viewsets.ViewSet):
             "nama_gudang": gudang.nama,
             "alamat_gudang": gudang.alamat,
             "kapasitas_gudang": gudang.kapasitas,
+            "jenis_gudang": gudang.jenis.nama,
             "barang": data
         }
 
